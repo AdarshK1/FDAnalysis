e04d36e13ab66ae2194cc1c82a59180565db5c2f
AdarshK1 <addy2999@gmail.com>
2016-01-22 00:02:52 -0500

    texts

---
+++ prog.py
@@ -0,0 +1,23 @@
+
+#Todo functions
+def cleantext(txt):     #get rid of html markup and punctuation
+    print("asfd")
+def txttoarray(txt):    #put all words into an array
+    print("asdf")
+def tally(txt):         #create a frequency table
+    print("asdf")
+
+
+fd = open("FDText.txt")
+hf = open("huckfinn.txt")
+
+fdText=fd.read()
+hfText=hf.read()
+
+fd.close()
+hf.close()
+
+
+
+
+
