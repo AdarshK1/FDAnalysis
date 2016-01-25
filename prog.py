4e6fa0cfdc6076625c24b999087c34573020c8d7
AdarshK1 <addy2999@gmail.com>
2016-01-24 21:05:01 -0500

    adf

--- prog.py
+++ prog.py
@@ -1,27 +1,41 @@
 
 #Todo functions
 def cleantext(txt):     #get rid of html markup and punctuation
-    print("asfd")
-def txttoarray(txt):    #put all words into an array
-    print("asdf")
-def tally(txt):         #create a frequency table
+    arr = []
+    temp = ""
+    for word in txt:
+        for char in word:
+            if (char not in punctuation):
+                temp = temp + char
+        temp = temp.rstrip()
+        temp = temp.lower()
+        arr . append (temp)
+        temp = ""
+    return arr
+
+
+
+def tallyWord(txt):         #create a frequency table
     print("asdf")
 
 
+punctuation = ",./?'|}{[]=+-_;:<>!@#$%^&*()`~"
 fd = open("FDText.txt")
 hf = open("huckfinn.txt")
 
 fdText=fd.read()
 hfText=hf.read()
 
+fdArray = fdText.split()
+hfArray = hfText.split()
 
-
-
+fdArray2 = cleantext(fdArray)
+hfArray2 = cleantext(hfArray)
 
 fd.close()
 hf.close()
 
-
+print(hfArray2)
 
 
 
